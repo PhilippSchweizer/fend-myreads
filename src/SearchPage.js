1cd66becd0cc5e0ff6d9726b6d3bef054d4345b8
PhilippSchweizer <ph.m.schweizer@outlook.com>
2018-08-19 14:33:36 +0200

    enable book search

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,18 +1,60 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI.js'
+import Book from './Book.js';
 
 class SearchPage extends Component {
+  state = {
+    query: '',
+    bookSearch: []
+  }
+
+  updateQuery = (query) => {
+    this.setState({
+      query: query
+    })
+    this.updateBooks(query)
+  }
+
+  updateBooks = (query) => {
+    if (query) {
+      BooksAPI.search(query).then(
+        (bookSearch) => {
+          if (bookSearch.error) {
+            this.setState({ bookSearch: [] })
+          } else {
+            this.setState({ bookSearch })
+          }
+        }
+      )
+    } else {
+      this.setState({ bookSearch: [] })
+    }
+  }
+
   render() {
     return (
       <div className="search-books">
         <div className="search-books-bar">
           <Link className="close-search" to="/">Close</Link>
           <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author" />
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.state.query}
+              onChange={(event) => this.updateQuery(event.target.value)} />
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+            {this.state.bookSearch.map(book => (
+              <li key={book.id}>
+                <Book
+                  book={book}
+                  books={this.state.bookSearch} />
+              </li>
+            ))}
+          </ol>
         </div>
       </div>
     )
