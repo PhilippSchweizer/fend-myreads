b5ff66fceed29f0004c17dca1091cf3a4d598f78
PhilippSchweizer <ph.m.schweizer@outlook.com>
2018-08-20 12:58:04 +0200

    fix: compare string with string instead object with string

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -5,27 +5,20 @@ class BookShelf extends Component {
   render() {
     const { books, shelf, changeShelf } = this.props
 
-   /*  let showingBooks
-    if (books.length === 0) {
-      return null;
-    } else {
-      showingBooks = books.filter(book => book.shelf === shelf)
-    } */
-
     return (
-      <div
-        className="bookshelf">
+      <div className="bookshelf">
         <h2 className="bookshelf-title">{shelf.name}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
             {books
-              .filter(book => book.shelf === shelf)
+              .filter(book => shelf.id === book.shelf)
               .map(book => (
-              <Book
-                book={book}
-                shelf={shelf.id}
-                changeShelf={changeShelf} />
-            ))}
+                <Book
+                  key={book.id}
+                  book={book}
+                  shelf={shelf.id}
+                  changeShelf={changeShelf} />
+              ))}
           </ol>
         </div>
       </div>
